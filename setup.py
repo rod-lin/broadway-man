6935e74f1542c09898e0f90ab1a79c8f5590a3ac
rod-lin <zl38@illinois.edu>
2019-02-11 05:53:09 +0800

    add dependencies

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
+import os
+
 from setuptools import setup, find_packages
+
+cwd = os.path.dirname(os.path.realpath(__file__))
+
+with open("{}/requirements.txt".format(cwd), "r") as f:
+    deps = f.readlines()
 
 setup(
     name="broadway-man",
     version="0.1",
-    packages=[ "broadway/man" ]
+    packages=[ "broadway/man" ],
+    install_requires=deps
 )
